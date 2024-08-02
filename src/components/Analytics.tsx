86052736c4f6cb384667a505c851475de37d78e1
Jimmy Loloy <loloy.laurencio@gmail.com>
2024-08-02 18:14:22 -0500

    FIX: typos in analytics

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
 
-import { GA_ADS_ID } from '../lib/googleAds';
 import * as gtag from '../lib/gtag';
 
 const App = () => {
@@ -21,25 +20,19 @@ const App = () => {
   return (
     <>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-49V3FCH958"></script>
+
+      <Script strategy="lazyOnload">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}', {
+            page_path: window.location.pathname,
             });
-          `,
-        }}
-      />
-      <Script async src={GA_ADS_ID} crossOrigin="anonymous" />
+          `}
+        </Script>
     </>
   );
 };
